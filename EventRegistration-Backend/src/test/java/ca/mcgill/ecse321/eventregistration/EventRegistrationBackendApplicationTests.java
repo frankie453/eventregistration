0be5de0970ee841c1f0aa4070edc0923ad60ba1e
anthony-ubah <anthon.ubah@mail.mcgill.ca>
2022-01-24 15:08:03 -0500

    New fix

--- EventRegistration-Backend/src/test/java/ca/mcgill/ecse321/eventregistration/EventRegistrationBackendApplicationTests.java
+++ EventRegistration-Backend/src/test/java/ca/mcgill/ecse321/eventregistration/EventRegistrationBackendApplicationTests.java
@@ -2,7 +2,7 @@ package ca.mcgill.ecse321.eventregistration;
 
 //import org.junit.jupiter.api.Test;
 //import org.springframework.boot.test.context.SpringBootTest;
-
+//New comment
 //@SpringBootTest
 class EventRegistrationBackendApplicationTests {
 
